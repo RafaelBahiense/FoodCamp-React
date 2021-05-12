3bdfca6da0722d26fe45fad599f06c6f24689186
ronald <ronaldjunior2@hotmail.com>
2021-05-12 15:51:52 -0300

    preparando review

--- src/components/menu/menu.js
+++
@@ -1,9 +0,0 @@
-import Section from './section/section';
-
-export default function Menu(props) {
-    return (
-        <div className={"menu"}>
-            {props.data.map((obj, section) => (obj.sectionIndex = section, obj.key = section, (<Section {...obj} />)))}
-        </div>
-    );
-}
--- src/components/navbar/navbar.js
+++
@@ -1,8 +0,0 @@
-export default function Navbar () {
-    return (
-            <div className={"header"}>
-                <div className={"title"}>FoodCamp</div>
-                <div className={"subtitle"}>Sua comida em 6 minutos</div>
-            </div>
-    );
-}
